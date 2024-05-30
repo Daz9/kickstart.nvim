d8edb031f5557a58c68c2f08e48764e0f9b62ab7
dakjoh <darren.johnston@sas.com>
2024-05-30 11:46:51 +0100

    sync

--- init.lua
+++ init.lua
@@ -470,6 +470,21 @@ require('lazy').setup {
     end,
   },
 
+  {
+    'ray-x/go.nvim',
+    dependencies = { -- optional packages
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('go').setup()
+    end,
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  },
+
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -618,11 +633,33 @@ require('lazy').setup {
               usePlaceholders = true,
               -- gofumpt = true,
               -- staticcheck = true,
-              golangcilint = true,
-              analyses = {
-                unusedparams = true,
-                unreachable = true,
-              },
+              -- golangcilint = true,
+              -- analyses = {
+              --   unusedparams = true,
+              --   unreachable = true,
+              -- },
+            },
+          },
+        },
+        golangci_lint_ls = {
+          init_options = {
+            command = {
+              'golangci-lint',
+              'run',
+              '--allow-parallel-runners',
+              '--disable-all',
+              '--enable=errcheck',
+              '--enable=gocritic',
+              '--enable=gosimple',
+              '--enable=govet',
+              '--enable=ineffassign',
+              '--enable=revive',
+              '-enable=staticcheck',
+              '--enable=typecheck',
+              '--enable=unused',
+              '--out-format',
+              'json',
+              '--issues-exit-code=1',
             },
           },
         },
@@ -918,6 +955,97 @@ require('lazy').setup {
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
+        incremental_selection = {
+          enable = enable,
+          keymaps = {
+            -- mappings for incremental selection (visual mappings)
+            init_selection = 'gnn', -- maps in normal mode to init the node/scope selection
+            node_incremental = 'grn', -- increment to the upper named parent
+            scope_incremental = 'grc', -- increment to the upper scope (as defined in locals.scm)
+            node_decremental = 'grm', -- decrement to the previous node
+          },
+        },
+
+        textobjects = {
+          -- syntax-aware textobjects
+          enable = enable,
+          lsp_interop = {
+            enable = enable,
+            peek_definition_code = {
+              ['DF'] = '@function.outer',
+              ['DF'] = '@class.outer',
+            },
+          },
+          keymaps = {
+            ['iL'] = {
+              -- you can define your own textobjects directly here
+              go = '(function_definition) @function',
+            },
+            -- or you use the queries from supported languages with textobjects.scm
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['aC'] = '@class.outer',
+            ['iC'] = '@class.inner',
+            ['ac'] = '@conditional.outer',
+            ['ic'] = '@conditional.inner',
+            ['ae'] = '@block.outer',
+            ['ie'] = '@block.inner',
+            ['al'] = '@loop.outer',
+            ['il'] = '@loop.inner',
+            ['is'] = '@statement.inner',
+            ['as'] = '@statement.outer',
+            ['ad'] = '@comment.outer',
+            ['am'] = '@call.outer',
+            ['im'] = '@call.inner',
+          },
+          move = {
+            enable = enable,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']m'] = '@function.outer',
+              [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+              [']M'] = '@function.outer',
+              [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[m'] = '@function.outer',
+              ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[M'] = '@function.outer',
+              ['[]'] = '@class.outer',
+            },
+          },
+          select = {
+            enable = enable,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+              -- Or you can define your own textobjects like this
+              ['iF'] = {
+                python = '(function_definition) @function',
+                cpp = '(function_definition) @function',
+                c = '(function_definition) @function',
+                java = '(method_declaration) @function',
+                go = '(method_declaration) @function',
+              },
+            },
+          },
+          swap = {
+            enable = enable,
+            swap_next = {
+              ['<leader>a'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>A'] = '@parameter.inner',
+            },
+          },
+        },
       }
 
       -- There are additional nvim-treesitter modules that you can use to interact
