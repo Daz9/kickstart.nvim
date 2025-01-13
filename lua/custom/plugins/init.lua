c1058eca004dae6061a30b3cece94a10b76a2aae
dakjoh <darren.johnston@sas.com>
2025-01-13 15:04:33 +0000

    update with avante nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -30,118 +30,62 @@ return {
     event = { 'CmdlineEnter' },
     ft = { 'go', 'gomod' },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    keys = {
+      { '<leader>xf', '<cmd>GoTestFunc<CR>', desc = 'Go test func' },
+      { '<leader>xF', '<cmd>GoTestFile<CR>', desc = 'Go test file' },
+    },
   },
   -- -- nvim avante setup:
-  -- {
-  --   'zbirenbaum/copilot.lua',
-  --   cmd = 'Copilot',
-  --   event = 'InsertEnter',
-  --   config = function()
-  --     require('copilot').setup {
-  --       -- suggestion = { enabled = false },
-  --       -- panel = { enabled = false },
-  --     }
-  --   end,
-  -- },
-  -- {
-  --   'zbirenbaum/copilot-cmp',
-  --   config = function()
-  --     require('copilot_cmp').setup()
-  --   end,
-  -- },
-  -- {
-  --   'yetone/avante.nvim',
-  --   event = 'VeryLazy',
-  --   lazy = false,
-  --   version = false, -- set this if you want to always pull the latest change
-  --   opts = {
-  --     -- add any opts here
-  --     provider = 'copilot',
-  --     behavior = {
-  --       auto_suggestions = false,
-  --     },
-  --   },
-  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  --   build = 'make',
-  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  --   dependencies = {
-  --     'stevearc/dressing.nvim',
-  --     'nvim-lua/plenary.nvim',
-  --     'MunifTanjim/nui.nvim',
-  --     --- The below dependencies are optional,
-  --     'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
-  --     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-  --     'zbirenbaum/copilot.lua', -- for providers='copilot'
-  --     {
-  --       -- support for image pasting
-  --       'HakonHarnes/img-clip.nvim',
-  --       event = 'VeryLazy',
-  --       opts = {
-  --         -- recommended settings
-  --         default = {
-  --           embed_image_as_base64 = false,
-  --           prompt_for_file_name = false,
-  --           drag_and_drop = {
-  --             insert_mode = true,
-  --           },
-  --           -- required for Windows users
-  --           use_absolute_path = true,
-  --         },
-  --       },
-  --     },
-  --     {
-  --       -- Make sure to set this up properly if you have lazy=true
-  --       'MeanderingProgrammer/render-markdown.nvim',
-  --       opts = {
-  --         file_types = { 'markdown', 'Avante' },
-  --       },
-  --       ft = { 'markdown', 'Avante' },
-  --     },
-  --   },
-  -- },
-
-  -- copilot chat
-  -- {
-  --   'CopilotC-Nvim/CopilotChat.nvim',
-  --   dependencies = {
-  --     { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
-  --     { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
-  --   },
-  --   opts = {
-  --     -- See Configuration section for options
-  --     mappings = {
-  --       complete = {
-  --         -- weird issue where the default tab mapping is stopping us from
-  --         -- using tab to select the copilot suggesting in the chat window
-  --         normal = '<C-t>',
-  --         insert = '<C-t>',
-  --       },
-  --     },
-  --   },
-  --   keys = {
-  --     { '<leader>acv', '<cmd>CopilotChatToggle<cr>', desc = 'CopilotChat - Explain code' },
-  --     -- Quick chat with Copilot
-  --     {
-  --       '<leader>acq',
-  --       function()
-  --         local input = vim.fn.input 'Quick Chat: '
-  --         if input ~= '' then
-  --           require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
-  --         end
-  --       end,
-  --       desc = 'CopilotChat - Quick chat',
-  --     },
-  --     -- -- Show prompts actions with telescope
-  --     -- {
-  --     --   '<leader>acp',
-  --     --   function()
-  --     --     local actions = require 'CopilotChat.actions'
-  --     --     require('CopilotChat.integrations.telescope').pick(actions.prompt_actions())
-  --     --   end,
-  --     --   desc = 'CopilotChat - Prompt actions',
-  --     -- },
-  --   },
-  -- },
+  {
+    'yetone/avante.nvim',
+    event = 'VeryLazy',
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      -- add any opts here
+      provider = 'copilot',
+      behavior = {
+        auto_suggestions = false,
+      },
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = 'make',
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      'stevearc/dressing.nvim',
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      --- The below dependencies are optional,
+      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+      'zbirenbaum/copilot.lua', -- for providers='copilot'
+      {
+        -- support for image pasting
+        'HakonHarnes/img-clip.nvim',
+        event = 'VeryLazy',
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { 'markdown', 'Avante' },
+        },
+        ft = { 'markdown', 'Avante' },
+      },
+    },
+  },
   -- copilot
   {
     'zbirenbaum/copilot.lua',
@@ -159,4 +103,24 @@ return {
       }
     end,
   },
+
+  -- copilot chat
+  -- {
+  --   'CopilotC-Nvim/CopilotChat.nvim',
+  --   tag = 'v3.3.2',
+  --   dependencies = {
+  --     { 'github/copilot.vim' }, -- or github/copilot.vim
+  --     { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+  --     { 'nvim-telescope/telescope.nvim' }, -- Use telescope for help actions
+  --   },
+  --   opts = {
+  --     show_help = true, -- Show help actions
+  --     auto_follow_cursor = false, -- Don't follow the cursor after getting response
+  --   },
+  --   event = 'VeryLazy',
+  --   keys = {
+  --     -- Toggle Copilot Chat Vsplit
+  --     { '<leader>acv', '<cmd>CopilotChatToggle<cr>', desc = 'CopilotChat - Toggle Vsplit' },
+  --   },
+  -- },
 }
